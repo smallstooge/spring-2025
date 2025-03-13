d5d1a116f991f1edcf85292c2801bdce33034f0c
smallstooge <steven.fisher30@buc.blinn.edu>
2025-03-13 01:22:36 -0500

    l

--- components/js/header.js
+++ components/js/header.js
@@ -1,25 +1,35 @@
 class Header extends HTMLElement {
     constructor() {
-        super();
+      super();
     }
+  
+
 
     connectedCallback() {
+
+
         fetch('components/header.html')
         .then(response => {
-            console.log('Fetch response:', response);
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.text();
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.text(); // or response.json() if it's a JSON file
         })
         .then(data => {
-            console.log('Fetched document content:', data);
-            this.innerHTML = data;
+          console.log('Document content:', data);
+          // Further processing of the document content
+        
+          var globalData = data;
+          this.innerHTML = globalData;
+        
         })
         .catch(error => {
-            console.error('Error fetching header:', error);
+          console.error('There was a problem fetching the document:', error);
         });
-    }
-}
 
-customElements.define('header-component', Header);
+
+      
+    }
+  }
+  
+  customElements.define('header-component', Header);
