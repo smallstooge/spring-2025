83fc177bdf13de7b8b44091283dd4afd1a153a81
smallstooge <steven.fisher30@buc.blinn.edu>
2025-03-13 01:15:01 -0500

    added fetch code

--- components/js/header.js
+++ components/js/header.js
@@ -3,53 +3,32 @@ class Header extends HTMLElement {
       super();
     }
   
+
+
     connectedCallback() {
-      this.innerHTML = `
 
 
-   <header>
-      <!-- begin navbar -->
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <!--<a class="navbar-brand" href="#">Navbar</a>-->
-          <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-            <ul class="navbar-nav mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about-us.html">About Us</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Dropdown </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider" /></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
+        fetch('/components/header.html')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.text(); // or response.json() if it's a JSON file
+        })
+        .then(data => {
+          console.log('Document content:', data);
+          // Further processing of the document content
+        
+          var globalData = data;
+          this.innerHTML = globalData;
+        
+        })
+        .catch(error => {
+          console.error('There was a problem fetching the document:', error);
+        });
 
-            <!--
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-            -->
-          </div>
-        </div>
-      </nav>
-      <!-- end navbar -->
-    </header>
 
-            `;
+      
     }
   }
   
